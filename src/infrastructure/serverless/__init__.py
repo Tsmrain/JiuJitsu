d6e7746b97ee9bd3ae781594de65deba5fff11b4
Santiago <01hanssantiago@gmail.com>
2026-09-02 10:13:14 -0400

    chore: remove entire project codebase and repository files

--- src/infrastructure/serverless/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Módulo serverless para Huawei Cloud FunctionGraph."""
-
-from src.infrastructure.serverless.container_app import app
-from src.infrastructure.serverless.functiongraph_handler import handler
-
-__all__ = ["app", "handler"]
-
